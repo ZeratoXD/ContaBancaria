a45f5055532f9024f8b448ebeb0b19ee7f0d025a
Ivan Dias <ZeratoXD>
2024-03-04 15:24:47 -0300

    main

---
+++ src/conta/Metodos_Conta.java
@@ -0,0 +1,14 @@
+package conta;
+
+public class Metodos_Conta {
+	
+	public static void sobre() {
+		System.out.println("\n*********************************************************");
+		System.out.println("Projeto Desenvolvido por: ");
+		System.out.println("Generation Brasil - generation@generation.org");
+		System.out.println("github.com/conteudoGeneration");
+		System.out.println("*********************************************************");
+	}
+}
+
+
